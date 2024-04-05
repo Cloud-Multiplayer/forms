6315c0144bd29c3d6e3f0046cf504c21ddfdcf9a
Elijah De Guzman <elijahdeguzman@Elijahs-MBP.attlocal.net>
2024-04-05 18:36:54 -0400

    forms files

--- java.js
+++ java.js
@@ -1,21 +1,16 @@
-var wins = 0, losses = 0, ties = 0;
-        
-function play(userChoice) {
-        var choices = ['rock', 'paper', 'scissors'];
-        var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+var todos = [];
+var todoForm = document.getElementById('todo-form');
 
-        if (userChoice === computerChoice) {
-            ties++;
-            document.getElementById('ties').innerText = ties;
-        } else if (
-            (userChoice === 'rock' && computerChoice === 'scissors') ||
-            (userChoice === 'paper' && computerChoice === 'rock') ||
-            (userChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-            wins++;
-            document.getElementById('wins').innerText = wins;
-        } else {
-            losses++;
-            document.getElementById('losses').innerText = losses;
-        }
-}
+var todoList = document.getElementById('todo-list');
+
+todoForm.onsubmit = function(e) {
+    e.preventDefault();
+    var newTodo = document.getElementById('new-todo').value;
+    todos.push(newTodo);
+    
+    var todoItems = todos.map(function(todo) {
+        return '<li>' + todo + '</li>';
+    });
+    todoList.innerHTML = todoItems.join('');
+    document.getElementById('new-todo').value = '';
+};
